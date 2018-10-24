0c62e0944e5852cc2827b50ceeaeec764cc45899
haydenbetts <haydenbetts@gmail.com>
2018-10-23 20:45:24 -0700

    [prog] added team names for mockData util

---
+++ utils/mockData.js
@@ -0,0 +1,37 @@
+/*
+
+I want to fake the following fields:
+
+We have the following schema:
+
+1 - 10 = basketball shoes
+11-20 = football shoes 
+21-30 = sandles and slides
+31-40 = soccer
+41-50 = original shoes
+51-60 = hoodies and sweater
+61-70 = running shoes
+71-80 = pants
+81-90 = bags and backpacks
+91-100 = hats and beanies
+
+I want to fake the following fields:
+
+
+“Name” - string
+“Price” - int
+“salePrice” - int
+“Gender” - [Men, Women, Children]
+“Category” - [Shoes, Clothing, Accessories]
+“Size” - [] - this is hard because it depends 
+“Sport” : [Originals, Running, Soccer, Basketball, Football, Skateboarding, Workout, Essentials, Sandals & Slides, Hiking, Golf, Tennis, Baseball]
+“Franchise” - string
+“Color” - string - can easily use faker for this
+“Partner” - string
+“Team” - string
+“Brand” - string
+"imageUrl" - string
+
+
+
+*/
