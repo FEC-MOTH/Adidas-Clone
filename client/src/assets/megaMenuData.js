75fed74fa07beb71bc69300fa18f4f0d42e62a41
Hack Reactor Student <student@hackreactor.com>
2018-10-26 16:40:34 -0700

    [progress] fully static men's mega-menu tab

---
+++ client/src/assets/megaMenuData.js
@@ -0,0 +1,98 @@
+module.exports = {
+    menMainMenuData: {
+        "Categories": [
+            {
+                "name": "FEATURED",
+                "subCategories": [
+                    "New Arrivals",
+                    "Bestsellers",
+                    "Release Dates",
+                    null,
+                    "P.O.D. System",
+                    "Ultraboost",
+                    "Ultraboost A16+",
+                    "NMD",
+                    "Fall Collection",
+                    null,
+                    {"name": "SALE", "emphasized": true}
+                ]
+            },
+            {
+                "name": "SHOES",
+                "subCategories": [
+                    "Originals",
+                    "Running",
+                    "Soccer",
+                    "Basketball",
+                    "Football",
+                    "Skateboarding",
+                    "Workout",
+                    "Essentials",
+                    "Sandals & Slides",
+                    "Hiking",
+                    "Golf",
+                    "Tennis",
+                    "Baseball",
+                    "Customize with miadidas",
+                    "Sale Shoes"
+                ]
+            },
+            {
+                "name": "Clothing",
+                "subCategories": [
+                    "Pants",
+                    "Hoodies & Sweatshirts",
+                    "Jackets & Vests",
+                    "Track Suits",
+                    "Short Sleeve Shirts",
+                    "Graphic T-Shirts",
+                    "Long Sleeve Shirts",
+                    "Polos",
+                    "Jerseys",
+                    "Compression",
+                    "Tights",
+                    "Shorts",
+                    "Tank Tops",
+                    "Underwear",
+                    "Sale Apparel"
+                ]
+            },
+            {
+                "name": "ACCESSORIES",
+                "subCategories": [
+                    "Bags & Backpacks",
+                    "Hats & Beanies",
+                    "Socks",
+                    "Phone Cases",
+                    "Sunglasses",
+                    "Balls",
+                    "Watches",
+                    "Gloves",
+                    "Scarves",
+                    "All Accessories",
+                    "Sale Accessories"
+                ]
+            },
+            {"name": "SPORTS",
+            "subCategories": [
+                "Running",
+                "Soccer",
+                "Basketball",
+                "Football",
+                "Skateboarding",
+                "Training",
+                "Outdoor",
+                "Tennis",
+                "Golf",
+                "Hockey",
+                "Baseball",
+                "Lacrosse",
+                "Volleyball",
+                "Weightlifting",
+                "College Gear"
+            ]
+        }
+        ]
+    }
+}
+
--- client/src/components/BottomHeader.jsx
+++ client/src/components/BottomHeader.jsx
@@ -1,31 +1,81 @@
 import React from 'react';
 import styles from '../css/BottomHeader.css'
+import { menMainMenuData } from '../assets/megaMenuData';
 
 const BottomHeader = (props) => {
   return (
     <div classNameName="glass-header-bottom-desktop">
       <ul className="menu-main">
         <li><a href="#">Men</a>
-        <div className="menu-sub">
-        <li>
-          <h3 className="menu-category"> FEATURED </h3>
-          <ul>
-            <li><a href="#">New Arrivals</a></li>
-            <li><a href="#">Bestsellers</a></li>
-            <li><a href="#">Release Dates</a></li>
-            <li className="horizontal-separator"></li>
-          </ul>
-         </li>
-        </div>
-      </li>
-      <li><a href="#">Women</a></li>
-    <li><a href="#">Kids</a></li >
-      <div className="menu-separator"> </div>
-      <li><a href="#">Sports</a></li >
+          <div className="menu-sub">
+
+            <div className="menu-sub-col">
+              <h3 className="menu-category"> FEATURED </h3>
+              <ul>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Bestsellers</a></li>
+                <li><a href="#">Release Dates</a></li>
+                <li className="horizontal-separator"></li>
+                <li><a href="#">P.O.D.System</a></li>
+                <li><a href="#">Ultraboost</a></li>
+                <li><a href="#">Ultraboost A16+</a></li>
+                <li><a href="#">NMD</a></li>
+                <li><a href="#">Fall Collection</a></li>
+                <li className="horizontal-separator"></li>
+                <li className="emphasized"><a href="#">SALE</a></li>
+              </ul>
+            </div>
+
+            <div className="menu-sub-col">
+              <h3 className="menu-category"> SHOES </h3>
+              <ul>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Bestsellers</a></li>
+                <li><a href="#">Release Dates</a></li>
+                <li className="horizontal-separator"></li>
+              </ul>
+            </div>
+
+            <div className="menu-sub-col">
+              <h3 className="menu-category"> CLOTHING </h3>
+              <ul>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Bestsellers</a></li>
+                <li><a href="#">Release Dates</a></li>
+                <li className="horizontal-separator"></li>
+              </ul>
+            </div>
+
+             <div className="menu-sub-col">
+              <h3 className="menu-category"> ACCESSORIES </h3>
+              <ul>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Bestsellers</a></li>
+                <li><a href="#">Release Dates</a></li>
+                <li className="horizontal-separator"></li>
+              </ul>
+            </div>
+
+             <div className="menu-sub-col">
+              <h3 className="menu-category"> SPORTS </h3>
+              <ul>
+                <li><a href="#">New Arrivals</a></li>
+                <li><a href="#">Bestsellers</a></li>
+                <li><a href="#">Release Dates</a></li>
+                <li className="horizontal-separator"></li>
+              </ul>
+            </div>
+
+          </div>
+        </li>
+        <li><a href="#">Women</a></li>
+        <li><a href="#">Kids</a></li >
+        <div className="menu-separator"> </div>
+        <li><a href="#">Sports</a></li >
         <li><a href="#">Brands</a></li >
-          <div className="menu-separator"> </div>
-          <li><a href="#">Customize</a></li >
-    </ul >
+        <div className="menu-separator"> </div>
+        <li><a href="#">Customize</a></li >
+      </ul >
     </div >
   )
 }
