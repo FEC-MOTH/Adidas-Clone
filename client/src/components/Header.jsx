b7da419d54bc9f79dd63ff8b5e4e4af2dc9c23ba
haydenbetts <haydenbetts@gmail.com>
2018-10-25 17:24:06 -0700

    [progress] work to mock the adidas static glass-header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,14 @@ import styles from '../css/header.css'
 
 const Header = (props) => {
   return (
-    <div>
-      <span className="header-desktop">A test paragraph</span>
+    <div className="header-desktop">
+      <div className="glass-header-top-desktop navigation">Here is the glass header
+          <a> Help </a>
+        <div className="menu-spacer"></div>
+        <a> Order Tracking and Returns </a>
+        <a> Newsletter Signup </a>
+        <a> Login </a>
+      </div>
     </div>)
 }
 
