090c2ecfa5e313ef9e1031f33b271971631412ba
Hack Reactor Student <student@hackreactor.com>
2018-10-26 17:07:32 -0700

    [progress] dynamically rendering men mega-menu based on JSON

--- client/src/components/BottomHeader.jsx
+++ client/src/components/BottomHeader.jsx
@@ -4,77 +4,41 @@ import { menMainMenuData } from '../assets/megaMenuData';
 
 const BottomHeader = (props) => {
   return (
-    <div classNameName="glass-header-bottom-desktop">
+    <div className="glass-header-bottom-desktop">
       <ul className="menu-main">
+      
         <li><a href="#">Men</a>
           <div className="menu-sub">
+            {menMainMenuData.Categories.map((category) => {
+              const jsx = (<div className="menu-sub-col">
+                <h3 className="menu-category"> {category.name} </h3>
+                <ul>
+                  {category.subCategories.map((subCategory) => {
+                    if (typeof subCategory === 'string') {
+                      return <li><a href="#">{subCategory}</a></li>;
+                    } else if (typeof subCategory === 'object' && subCategory !== null) {
+                      return <li className="emphasized" ><a href="#">{subCategory.name}</a></li>;
+                    } else if (subCategory === null) {
+                      return <li className="horizontal-separator"></li>;
+                    }
+                  })}
+                </ul>
+              </div>
+              )
+              return jsx;
+            }
+          )}
+        </div>
 
-            <div className="menu-sub-col">
-              <h3 className="menu-category"> FEATURED </h3>
-              <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Bestsellers</a></li>
-                <li><a href="#">Release Dates</a></li>
-                <li className="horizontal-separator"></li>
-                <li><a href="#">P.O.D.System</a></li>
-                <li><a href="#">Ultraboost</a></li>
-                <li><a href="#">Ultraboost A16+</a></li>
-                <li><a href="#">NMD</a></li>
-                <li><a href="#">Fall Collection</a></li>
-                <li className="horizontal-separator"></li>
-                <li className="emphasized"><a href="#">SALE</a></li>
-              </ul>
-            </div>
+      </li>
 
-            <div className="menu-sub-col">
-              <h3 className="menu-category"> SHOES </h3>
-              <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Bestsellers</a></li>
-                <li><a href="#">Release Dates</a></li>
-                <li className="horizontal-separator"></li>
-              </ul>
-            </div>
-
-            <div className="menu-sub-col">
-              <h3 className="menu-category"> CLOTHING </h3>
-              <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Bestsellers</a></li>
-                <li><a href="#">Release Dates</a></li>
-                <li className="horizontal-separator"></li>
-              </ul>
-            </div>
-
-             <div className="menu-sub-col">
-              <h3 className="menu-category"> ACCESSORIES </h3>
-              <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Bestsellers</a></li>
-                <li><a href="#">Release Dates</a></li>
-                <li className="horizontal-separator"></li>
-              </ul>
-            </div>
-
-             <div className="menu-sub-col">
-              <h3 className="menu-category"> SPORTS </h3>
-              <ul>
-                <li><a href="#">New Arrivals</a></li>
-                <li><a href="#">Bestsellers</a></li>
-                <li><a href="#">Release Dates</a></li>
-                <li className="horizontal-separator"></li>
-              </ul>
-            </div>
-
-          </div>
-        </li>
-        <li><a href="#">Women</a></li>
-        <li><a href="#">Kids</a></li >
-        <div className="menu-separator"> </div>
-        <li><a href="#">Sports</a></li >
-        <li><a href="#">Brands</a></li >
-        <div className="menu-separator"> </div>
-        <li><a href="#">Customize</a></li >
+      <li><a href="#">Women</a></li>
+      <li><a href="#">Kids</a></li >
+      <div className="menu-separator"> </div>
+      <li><a href="#">Sports</a></li >
+      <li><a href="#">Brands</a></li >
+      <div className="menu-separator"> </div>
+      <li><a href="#">Customize</a></li >
       </ul >
     </div >
   )
