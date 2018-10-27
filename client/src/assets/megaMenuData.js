a1682161410add78098970d5a1f833a8471f5af3
Hack Reactor Student <student@hackreactor.com>
2018-10-26 17:41:01 -0700

    [progress] rendering kids menu

--- client/src/assets/megaMenuData.js
+++ client/src/assets/megaMenuData.js
@@ -15,7 +15,7 @@ module.exports = {
                     "NMD",
                     "Fall Collection",
                     null,
-                    {"name": "SALE", "emphasized": true}
+                    { "name": "SALE", "emphasized": true }
                 ]
             },
             {
@@ -74,26 +74,192 @@ module.exports = {
                     "Sale Accessories"
                 ]
             },
-            {"name": "SPORTS",
-            "subCategories": [
-                "Running",
-                "Soccer",
-                "Basketball",
-                "Football",
-                "Skateboarding",
-                "Training",
-                "Outdoor",
-                "Tennis",
-                "Golf",
-                "Hockey",
-                "Baseball",
-                "Lacrosse",
-                "Volleyball",
-                "Weightlifting",
-                "College Gear"
-            ]
-        }
+            {
+                "name": "SPORTS",
+                "subCategories": [
+                    "Running",
+                    "Soccer",
+                    "Basketball",
+                    "Football",
+                    "Skateboarding",
+                    "Training",
+                    "Outdoor",
+                    "Tennis",
+                    "Golf",
+                    "Hockey",
+                    "Baseball",
+                    "Lacrosse",
+                    "Volleyball",
+                    "Weightlifting",
+                    "College Gear"
+                ]
+            }
         ]
-    }]
+    },
+    {
+        "name": "Women",
+        "Categories": [
+            {
+                "name": "FEATURED",
+                "subCategories": [
+                    "New Arrivals",
+                    "Best Sellers",
+                    "Release Dates",
+                    "null",
+                    "P.O.D.System",
+                    "Ultraboost",
+                    "NMD",
+                    "Statement Collection",
+                    "Fall Collection",
+                    null,
+                    { "name": "SALE", "emphasized": true }
+                ]
+            },
+            {
+                "name": "SHOES",
+                "subCategories": [
+                    "Originals",
+                    "Running",
+                    "Workout",
+                    "Essentials",
+                    "Sandals & Slides",
+                    "Hiking",
+                    "Tennis",
+                    "Golf",
+                    "Soccer",
+                    "Volleyball",
+                    "Customize with miadidas",
+                    "Sale Shoes"
+                ]
+            },
+            {
+                "name": "CLOTHING",
+                "subCategories": [
+                    "Hoodies & Sweatshirts",
+                    "Pants",
+                    "Tights",
+                    "Sports Bras",
+                    "Track Suits",
+                    "Jackets & Vests",
+                    "Short Sleeve Shirts",
+                    "Long Sleeve Shirts",
+                    "Compression",
+                    "Polos",
+                    "Dresses & Skirts",
+                    "Shorts",
+                    "Tank Tops",
+                    "Sale Apparel"
+                ]
+            },
+            {
+                "name": "ACCESSORIES",
+                "subCategories": [
+                    "Bags & Backpacks",
+                    "Hats & Beanies",
+                    "Socks",
+                    "Headbands",
+                    "Phone Cases",
+                    "Sunglasses",
+                    "Balls",
+                    "Watches",
+                    "All Accessories",
+                    "Sale Accessories"
+
+                ]
+            },
+            {
+                "name": "SPORTS",
+                "subCategories": [
+                    "Running",
+                    "Training",
+                    "Outdoor",
+                    "Tennis",
+                    "Golf",
+                    "Soccer",
+                    "Volleyball",
+                    "Yoga",
+                    "Lacrosse",
+                    "Hockey",
+                    "College Gear"
+                ]
+            }
+        ]
+    },
+
+
+
+
+    {
+        "name": "Kids",
+        "Categories": [
+            {
+                "name": "FEATURED",
+                "subCategories": [
+                    "New Arrivals",
+                    "Best Sellers",
+                    null,
+                    "P.O.D.System",
+                    "Ultraboost",
+                    "NMD",
+                    "adicolor",
+                    "Fall Collection",
+                    null,
+                    { "name": "SALE", "emphasized": true }]
+            },
+            {
+                "name": "YOUTH (8-14)",
+                "subCategories": [
+                    "Boys Shoes",
+                    "Girls Shoes",
+                    "Originals Shoes",
+                    "Sport Shoes",
+                    null,
+                    "Boys Clothing",
+                    "Girls Clothing",
+                    "Hoodies & Sweatshirts",
+                    "Pants & Shorts",
+                    null,
+                    "Sale",
+                    null]
+            },
+            {
+                "name": "CHILDREN (4-8)",
+                "subCategories": [
+                    "Boys Shoes",
+                    "Girls Shoes",
+                    "Originals Shoes",
+                    "Sport Shoes",
+                    null,
+                    "Boys Clothing",
+                    "Girls Clothing",
+                    null,
+                    "Sale",
+                    null,
+                    { "name": "all CHILDREN", "emphasized": true }]
+            },
+            {
+                "name": "BABY & TODDLER (0-4)",
+                "subCategories": [
+                    "Shoes",
+                    "Clothing",
+                    null,
+                    "Sale",
+                    null,
+                    { "name": "all Baby & Toddler", "emphasized": true }]
+            },
+            {
+                "name": "ACCESSORIES",
+                "subCategories": [
+                    "Bags & Backpacks",
+                    "Lunch Bags",
+                    "Socks",
+                    "Hats & Beanies",
+                    "Balls"
+                ]
+            }
+        ]
+    }
+
+    ]
 }
 
