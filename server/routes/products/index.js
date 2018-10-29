435dc7e618321e45b81addcad47d669a80bcaa60
haydenbetts <haydenbetts@gmail.com>
2018-10-29 15:15:07 -0700

    [progress] returning 10 autocomplete results at the appropriate endpoint

--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const sequelize = require('sequelize');
 const { Product } = require('../../../database/models');
 const { connection } = require('../../../database/index');
-const sequelize = require('sequelize');
 const { sqlQuery } = require('../../../database/utils/autoCompleteSQL');
 
 const router = express.Router();
@@ -45,63 +45,36 @@ router.route('/search/suggestions')
 
     SELECT Count(*) FROM name, team, spot, category, color, gender
       WHERE name || team || sport || category || color || gender like query
-    
-    
+  */
 
-    */
-    // Product.findAll({
-    //   where: {
-    //     $or: [
-    //       { name: { $like: `%${query}%` } },
-    //       { team: { $like: `%${query}%` } },
-    //       { sport: { $like: `%${query}%` } },
-    //       { category: { $like: `%${query}%` } },
-    //       { color: { $like: `%${query}%` } },
-    //       { gender: { $like: `${query}%` } }
-    //     ]
-    //   },
-    //   attributes: ['name', 'team', 'sport', 'category', 'color', 'gender']
-    // })
     connection.query(sqlQuery(query), { type: sequelize.QueryTypes.SELECT })
       .then((responseArray) => {
 
+        const counts = {};
+        const keys = Object.keys(responseArray[0]);
 
-        // create full text search by concatenating all fields with some delimeter between them
-        // return all matching full text
-        // for each matching fulltext, 
-        // use a regex that splits on the delimeter
-        // and stores that word in an array
-        // then... count the number of each words and store
+        for (let i = 0; i < responseArray.length; i += 1) {
+          keys.forEach((key) => {
+            if (responseArray[i][key] !== null) {
+              if (counts[responseArray[i][key]]) {
+                counts[responseArray[i][key]] += 1;
+              } else {
+                counts[responseArray[i][key]] = 1;
+              }
+            }
+          })
+        }
+
+        const countsArray = [];
+
+        Object.keys(counts).forEach((key) => {
+          countsArray.push({ match: key, count: counts[key] });
+        })
+
+        countsArray.sort((aTuple, bTuple) => bTuple.count - aTuple.count)
 
 
-        // TODO - This is bad. Currently an autocomplete search for "ball"
-        // will return 10 results, 7 of which are "football". This is because
-        // all fields are simply stored as strings with no notion of relation
-        // where a substring matches, select the matching field
-        // count the number of fields that match
-
-        // right now I am returning a big object, which I am sorting using javascript
-        // this could be made much more efficient with a carefully constructed SQL query
-
-        // const counts = {};
-
-        // const possibleAttributes = ['name', 'team', 'sport', 'category', 'color', 'gender'];
-        // // create an array of all of the keys in the response
-        // for (let i = 0; i < responseArray.length; i += 1) {
-
-        //   for (let j = 0; j < possibleAttributes.length; j += 1) {
-        //     if (responseArray[i][possibleAttributes[j]] !== undefined) {
-        //       if (counts[responseArray[i][possibleAttributes[j]]] === undefined) {
-        //         counts[responseArray[i][possibleAttributes[j]]] = 1;
-        //       } else {
-        //         counts[responseArray[i][possibleAttributes[j]]] += 1;
-        //       }
-        //     }
-        //   } // [{name:"football", count:50}, {name:"basketball" }]
-        // }
-        // res.send(counts);
-
-        res.send(responseArray);
+        res.send(countsArray.slice(0, 10));
       })
   })
 
