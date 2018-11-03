586e4bccec051c18589ffaa8daa97e4ac5cca87b
haydenbetts <haydenbetts@gmail.com>
2018-11-03 12:40:24 -0700

    [progress] added the old sql suggestions query for benchmarking purposes

--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -46,6 +46,8 @@ router.route('/search/suggestions')
     SELECT Count(*) FROM name, team, spot, category, color, gender
       WHERE name || team || sport || category || color || gender like query
   */
+
+
     if (!!query === false) {
       res.send('');
     } else {
@@ -80,6 +82,51 @@ router.route('/search/suggestions')
           res.send(countsArray.slice(0, 10));
         })
     }
+
+
+    /* OLD QUERY used for benchmarking
+    Product.findAll({
+      where: {
+        $or: [
+          { name: { $like: `%${query}%` } },
+          { team: { $like: `%${query}%` } },
+          { sport: { $like: `%${query}%` } },
+          { category: { $like: `%${query}%` } },
+          { color: { $like: `%${query}%` } },
+          { gender: { $like: `${query}%` } }
+        ]
+      },
+      attributes: ['name', 'team', 'sport', 'category', 'color', 'gender']
+    }).then((responseArray) => {
+      // TODO - This is bad. Currently an autocomplete search for "ball"
+      // will return 10 results, 7 of which are "football". This is because
+      // all fields are simply stored as strings with no notion of relation
+      // where a substring matches, select the matching field
+      // count the number of fields that match
+
+      // right now I am returning a big object, which I am sorting using javascript
+      // this could be made much more efficient with a carefully constructed SQL query
+
+      const counts = {};
+
+      const possibleAttributes = ['name', 'team', 'sport', 'category', 'color', 'gender'];
+      // create an array of all of the keys in the response
+      for (let i = 0; i < responseArray.length; i++) {
+
+        for (let j = 0; j < possibleAttributes.length; j++) {
+          if (responseArray[i][possibleAttributes[j]] === undefined) {
+            continue;
+          }
+          if (counts[responseArray[i][possibleAttributes[j]]] === undefined) {
+            counts[responseArray[i][possibleAttributes[j]]] = 1;
+          } else {
+            counts[responseArray[i][possibleAttributes[j]]] += 1;
+          }
+        }
+      }
+      res.send(counts);
+    })
+    */
   })
 
 module.exports = router;
