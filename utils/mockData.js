09f22a2ebab65e365f2546c91065aac432f30cd2
haydenbetts <haydenbetts@gmail.com>
2018-11-04 08:07:22 -0800

    refactored shoeDecorator using closure so it only sorts sports arrays on first run

--- utils/mockData.js
+++ utils/mockData.js
@@ -1,7 +1,5 @@
 /*
-
-I want to fake the following fields:
-
+The following 
 We have the following schema:
 
 1 - 10 = basketball shoes
@@ -30,28 +28,30 @@ I want to fake the following fields:
 "gender" - string [Men, Women, Children]
 "category" - string [Shoes, Sandles and Slides, Hoodies and Sweater, Pants, Bags and Backpacks, Hats and Beanies]
 */
-const validProductPriceRange = { min: 50, max: 300 };
 const faker = require("faker");
 const { teamNames } = require("./staticDataForMock.js");
 
-const gendersForMock = ["Men", "Women", "Children"];
-const colorsForMock = ["Black", "white", "red", "green", "yellow", "blue", "pink", "gray", "brown", "orange", "purple"];
-const sportsForMock = ["Basketball", "Football", null, "Soccer", "Original"];
 const basketballAccessories = require('./data/basketball-accessories');
 const basketBallApparel = require('./data/basketball-apparel');
 const basketballShoes = require('./data/basketball-shoes');
-// const footballAccessories = require('./data/football-accessories');
-// const footballApparel = require('./data/football-apparel');
 const footballCleats = require('./data/football-cleats');
 const runningAccessories = require('./data/running-accessories');
-// const runningApparel = require('./data/running-apparel');
 const runningShoes = require('./data/running-shoes');
 
-
+const setUpForSampleDataGenerationComplete = false;
+const gendersForMock = ["Men", "Women", "Children"];
+const colorsForMock = ["Black", "white", "red", "green", "yellow", "blue", "pink", "gray", "brown", "orange", "purple"];
+const sportsForMock = ["Basketball", "Football", null, "Soccer", "Original"];
+const validProductPriceRange = { min: 50, max: 300 };
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+// need - teamNames filtered for 'nfl'
+// need -  teamNames filtered for 'mls'
+// need - teamNames filtered for 'nba'
+
 
 const generateImageUrl = (imgId, imgFileName) => `http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/${imgId}/zoom/${imgFileName}?sh=${64}`
 
@@ -107,120 +107,144 @@ const hoodiePantsBackpackHatDecorator = (product) => {
   return decoratedProduct;
 }
 
-const shoeDecorator = (product) => {
-  const shoe = product;
+const shoeDecorator = (() => {
+  let executed = false;
+  let nfl; let mls; let nba;
 
-  let shoeApiData;
-  let imgId;
-  let imgFileName;
+  return (product) => {
 
-  if (shoe.category === 'Shoe') {
-    shoe.sport = sportsForMock[getRandomInt(0, sportsForMock.length)];
+    if (!executed) {
+      executed = true;
+      nfl = teamNames.filter((team) => team.league === "nfl");
+      mls = teamNames.filter((team) => team.league === "mls");
+      nba = teamNames.filter((team) => team.league === "nba");
+    }
+
+    const shoe = product;
+
+    let shoeApiData;
+    let imgId;
+    let imgFileName;
+
+    if (shoe.category === 'Shoe') {
+      shoe.sport = sportsForMock[getRandomInt(0, sportsForMock.length)];
+    }
+
+    // TODO CURRENTLY WE ARE FILTERING A GIANT TEAMS ARRAY EVERY TIME THIS IS RUN
+    if (shoe.sport === "Football") {
+      // const nfl = teamNames.filter((team) => team.league === "nfl");
+      shoe.team = nfl[getRandomInt(0, nfl.length)].name;
+
+      shoeApiData = footballCleats[getRandomInt(0, footballCleats.length)];
+      imgId = shoeApiData.images[0].id;
+      imgFileName = shoeApiData.images[0].fileName;
+    } else if (shoe.sport === "Soccer") {
+      // const mls = teamNames.filter((team) => team.league === "mls");
+      shoe.team = mls[getRandomInt(0, mls.length)].name;
+
+      shoeApiData = footballCleats[getRandomInt(0, footballCleats.length)];
+      imgId = shoeApiData.images[0].id;
+      imgFileName = shoeApiData.images[0].fileName;
+    } else if (shoe.sport === "Basketball") {
+      // const nba = teamNames.filter((team) => team.league === "nba");
+      shoe.team = nba[getRandomInt(0, nba.length)].name;
+
+
+      shoeApiData = basketballShoes[getRandomInt(0, basketballShoes.length)];
+      imgId = shoeApiData.images[0].id;
+      imgFileName = shoeApiData.images[0].fileName;
+    } else {
+      shoe.team = null;
+
+      shoeApiData = runningShoes[getRandomInt(0, runningShoes.length)];
+      imgId = shoeApiData.images[0].id;
+      imgFileName = shoeApiData.images[0].fileName;
+    }
+
+    shoe.imageUrl = `http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/${imgId}/zoom/${imgFileName}?sh=${64}`;
+
+    // TODO DRY!
+    if (shoe.sport === 'Football' && shoe.team) {
+      shoe.name =
+        ` ${
+        shoe.team
+        } ${
+        shoe.sport
+        } ${
+        'Cleat'}`
+    }
+    else if (shoe.team) {
+      shoe.name =
+        ` ${
+        shoe.team
+        } ${
+        shoe.sport
+        } ${
+        shoe.category}`;
+    } else {
+      shoe.name =
+        `${faker.fake("{{name.firstName}}")
+        } ${
+        faker.fake("{{address.countryCode}}")
+        } ${
+        shoe.sport
+        } ${
+        shoe.category}`;
+    }
+
+    return shoe;
   }
-
-  // TODO CURRENTLY WE ARE FILTERING A GIANT TEAMS ARRAY EVERY TIME THIS IS RUN
-  if (shoe.sport === "Football") {
-    const nfl = teamNames.filter((team) => team.league === "nfl");
-    shoe.team = nfl[getRandomInt(0, nfl.length)].name;
-
-    shoeApiData = footballCleats[getRandomInt(0, footballCleats.length)];
-    imgId = shoeApiData.images[0].id;
-    imgFileName = shoeApiData.images[0].fileName;
-  } else if (shoe.sport === "Soccer") {
-    const mls = teamNames.filter((team) => team.league === "mls");
-    shoe.team = mls[getRandomInt(0, mls.length)].name;
-
-    shoeApiData = footballCleats[getRandomInt(0, footballCleats.length)];
-    imgId = shoeApiData.images[0].id;
-    imgFileName = shoeApiData.images[0].fileName;
-  } else if (shoe.sport === "Basketball") {
-    const nba = teamNames.filter((team) => team.league === "nba");
-    shoe.team = nba[getRandomInt(0, nba.length)].name;
+})();
 
 
-    shoeApiData = basketballShoes[getRandomInt(0, basketballShoes.length)];
-    imgId = shoeApiData.images[0].id;
-    imgFileName = shoeApiData.images[0].fileName;
-  } else {
-    shoe.team = null;
+const generateProduct = (() => {
 
-    shoeApiData = runningShoes[getRandomInt(0, runningShoes.length)];
-    imgId = shoeApiData.images[0].id;
-    imgFileName = shoeApiData.images[0].fileName;
-  }
+  let executed = false;
 
-  shoe.imageUrl = `http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/${imgId}/zoom/${imgFileName}?sh=${64}`;
+  return (category) => {
 
-  // TODO DRY!
-  if (shoe.sport === 'Football' && shoe.team) {
-    shoe.name =
-      ` ${
-      shoe.team
-      } ${
-      shoe.sport
-      } ${
-      'Cleat'}`
-  }
-  else if (shoe.team) {
-    shoe.name =
-      ` ${
-      shoe.team
-      } ${
-      shoe.sport
-      } ${
-      shoe.category}`;
-  } else {
-    shoe.name =
-      `${faker.fake("{{name.firstName}}")
-      } ${
-      faker.fake("{{address.countryCode}}")
-      } ${
-      shoe.sport
-      } ${
-      shoe.category}`;
-  }
+    if (!executed) {
+      executed = true;
+    }
 
-  return shoe;
-};
+    let product = {};
 
-const generateProduct = (category) => {
-  let product = {};
+    product.category = category;
 
-  product.category = category;
-
-  product.price = getRandomInt(
-    validProductPriceRange.min,
-    validProductPriceRange.max
-  );
-
-  // Add a sale price based on actual price 1/3 of the time
-  if (getRandomInt(0, 10) <= 3) {
-    product.salePrice = getRandomInt(
+    product.price = getRandomInt(
       validProductPriceRange.min,
-      product.price
+      validProductPriceRange.max
     );
-  } else {
-    product.salePrice = null;
+
+    // Add a sale price based on actual price 1/3 of the time
+    if (getRandomInt(0, 10) <= 3) {
+      product.salePrice = getRandomInt(
+        validProductPriceRange.min,
+        product.price
+      );
+    } else {
+      product.salePrice = null;
+    }
+
+    product.color = colorsForMock[getRandomInt(0, colorsForMock.length)];
+
+    // the random int generator is exclusive of the max (which is why we make max 6 here)
+    // TODO - ratings should have non-integer values
+    // TODO - come up with a distribution of ratings so there are more 4 and 5
+    product.rating = getRandomInt(0, 6);
+
+    product.num_ratings = getRandomInt(10, 500);
+
+    product.gender = gendersForMock[getRandomInt(0, gendersForMock.length)];
+
+    if (product.category === 'Shoe' || product.category === 'Sandle') {
+      product = shoeDecorator(product);
+    } else if (product.category === 'Hoodie' || product.category === 'Pants' || product.category === 'Backpack' || product.category === 'Hat') {
+      product = hoodiePantsBackpackHatDecorator(product);
+    }
+
+    return product;
   }
-
-  product.color = colorsForMock[getRandomInt(0, colorsForMock.length)];
-
-  // the random int generator is exclusive of the max (which is why we make max 6 here)
-  // TODO - ratings should have non-integer values
-  // TODO - come up with a distribution of ratings so there are more 4 and 5
-  product.rating = getRandomInt(0, 6);
-
-  product.num_ratings = getRandomInt(10, 500);
-
-  product.gender = gendersForMock[getRandomInt(0, gendersForMock.length)];
-
-  if (product.category === 'Shoe' || product.category === 'Sandle') {
-    product = shoeDecorator(product);
-  } else if (product.category === 'Hoodie' || product.category === 'Pants' || product.category === 'Backpack' || product.category === 'Hat') {
-    product = hoodiePantsBackpackHatDecorator(product);
-  }
-
-  return product;
-}
+})()
 
 module.exports.generateProduct = generateProduct;
