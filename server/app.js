6fd311d902892c254fd0eb9a7a97f542deb17d93
haydenbetts <haydenbetts@gmail.com>
2018-11-07 07:50:23 -0800

    [progress] added cors middleware

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
