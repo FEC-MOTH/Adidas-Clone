ef814fd76b6480822d3802db3e8d325f5765ad18
haydenbetts <haydenbetts@gmail.com>
2018-11-06 21:00:15 -0800

    added cors to component

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
