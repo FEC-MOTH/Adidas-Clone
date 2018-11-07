7d22f6cc12553cd87bffc6995d16ee4d9801c861
haydenbetts <haydenbetts@gmail.com>
2018-11-06 20:44:00 -0800

    removed all static assets

--- client/src/components/BottomHeader.jsx
+++ client/src/components/BottomHeader.jsx
@@ -7,7 +7,7 @@ import EmptyBagIcon from '../components/EmptyBagIcon';
 
 const BottomHeader = (props) => (
   <div className="glass-header-bottom-desktop">
-    <img id="logo" src="https://s3-us-west-1.amazonaws.com/shoedidas-static/assets/img/shoedidas_white_5.svg"></img>
+    <img id="logo" src={process.env.CDN_ROOT.concat('/assets/img/shoedidas_white_5.svg')} ></img>
     {/* <ShoedidasLogo id="logo" /> */}
     <ul className="menu-main">
       <MegaMenu megaMenuAssets={megaMenuAssets} />
