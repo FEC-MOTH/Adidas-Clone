ed3d4fd86055b7f8ef080b6fc04a2ba6b0318e64
haydenbetts <haydenbetts@gmail.com>
2018-12-02 08:46:08 -0800

    [progress] fixed issue with sale price display, added env variable for listen port, ignored .DS_STORE

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require("./app");
 
-const port = 80;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
